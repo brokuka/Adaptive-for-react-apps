fb5d685ae2aee03e0ea2e2cb8433ecabd9fd5f81
Ernest <erik.manukyan2010@yandex.ru>
2022-05-12 18:42:28 +0400

    add womazing

---
+++ womazing/src/assets/js/app.js
@@ -0,0 +1,16 @@
+'use strict';
+import Swiper, { Pagination } from 'swiper';
+
+const swiper = new Swiper('.swiper', {
+	slidesPerView: 1,
+	spaceBetween: 15,
+	modules: [Pagination],
+	pagination: {
+		el: ".swiper-pagination",
+		clickable: true,
+		renderBullet: function (index, className) {
+			return '<span class="' + className + '"></span>';
+			// return `<span class="${className}">${index + 1}</span> `;
+		},
+	},
+});
