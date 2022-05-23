2cba3a51172b8f42ef7b719c0985d298415113fc
Ernest Manukyan <erik.manukyan2010@yandex.ru>
2022-05-24 00:09:12 +0400

    changes and fixes

--- womazing/src/assets/js/components/slider.js
+++ womazing/src/assets/js/components/slider.js
@@ -1,11 +1,17 @@
-import Swiper, { Pagination, Navigation, Controller } from 'swiper';
+import Swiper, { Pagination, Navigation, Controller, Autoplay } from 'swiper';
 
 Swiper.use([Controller]);
 
 let introSliderText = new Swiper('.intro_slider_text', {
 	slidesPerView: 1,
 	spaceBetween: 15,
-	modules: [Pagination, Controller],
+	grabCursor: true,
+	autoplay: {
+		delay: 5000,
+		disableOnInteraction: false
+	},
+	loop: true,
+	modules: [Pagination, Controller, Autoplay],
 	pagination: {
 		el: ".swiper-pagination",
 		clickable: true,
@@ -15,14 +21,23 @@ let introSliderText = new Swiper('.intro_slider_text', {
 	},
 });
 
-let introSliderImage = new Swiper('.intro_slider_image', {});
+let introSliderImage = new Swiper('.intro_slider_image', {
+	grabCursor: true,
+	loop: true,
+});
 
 introSliderText.controller.control = introSliderImage;
 introSliderImage.controller.control = introSliderText;
 
 const dreamTeamSlider = new Swiper('.dream_team_slider', {
 	slidesPerView: 1,
-	modules: [Pagination, Navigation],
+	grabCursor: true,
+	modules: [Pagination, Navigation, Autoplay],
+	autoplay: {
+		delay: 5000,
+		disableOnInteraction: false
+	},
+	loop: true,
 	pagination: {
 		el: ".swiper-pagination",
 		clickable: true,
@@ -34,4 +49,9 @@ const dreamTeamSlider = new Swiper('.dream_team_slider', {
 		nextEl: ".swiper_button_next",
 		prevEl: ".swiper_button_prev",
 	},
+});
+
+const shopItemSlider = new Swiper('.shop_item_slider', {
+	spaceBetween: 15,
+	grabCursor: true
 });
